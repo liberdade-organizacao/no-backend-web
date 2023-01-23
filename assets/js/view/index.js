05a8a33b5830c06ba45a0249004060c49821de16
Cris Silva Jr <crisjr@pm.me>
2023-01-23 16:03:15 -0300

    Indicate structure to let clients list their apps

--- assets/js/view/index.js
+++ assets/js/view/index.js
@@ -1,3 +1,8 @@
+function buildAppsList(apps) {
+  // TODO build table with all apps
+  return "there are apps here";
+}
+
 function main() {
   // checks if client is logged in or not
   // if not, redirect to login page
@@ -14,8 +19,13 @@ function main() {
       alert(result.error);
       return;
     }
-
-    console.log(result["apps"]);
+  
+    const apps = result["apps"];
+    if (apps.length === 0) {
+      return;
+    }
+    var appsList = document.getElementById("apps-list");
+    appsList.innerHTML = buildAppsList(apps);
   });
 }
 
