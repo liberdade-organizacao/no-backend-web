8738eead0a5fce5005089664a0b889254a466f34
Cris Jr <crisjr@pm.me>
2023-01-25 09:19:38 -0300

    Generate more mock data: users

--- tests/index.js
+++ tests/index.js
@@ -11,8 +11,8 @@ function randomString(length) {
 function main() {
   var status = document.getElementById("status");
 
-  // const serverUrl = "http://localhost:7780";
-  const serverUrl = "http://192.168.0.18:7780";
+  const serverUrl = "http://localhost:7780";
+  // const serverUrl = "http://192.168.0.18:7780";
   signUp(serverUrl, "hello@crisjr.eng.br", "password", function(signUpResult) {
     console.log("--- # sign up");
     authKey = signUpResult["auth_key"];
@@ -46,10 +46,25 @@ function main() {
 	return;
       }
       var numberOfApps = 2 + 10 * Math.random();
-      for (var j = 0; j < numberOfApps; j++) {
+      for (var i2 = 0; i2 < numberOfApps; i2++) {
         createApp(serverUrl, signUpResult["auth_key"], randomString(3 + 5 * Math.random()), function(appCreationResult) {
 	  console.log("--- # create app");
 	  console.log(appCreationResult);
+	  var appAuthKey = appCreationResult["auth_key"];
+
+	  createUser(serverUrl, appAuthKey, "hello@crisjr.eng.br", "pwd", function(userCreationResult) {
+	    console.log("--- # creating user hello@crisjr.eng.br");
+	    console.log(userCreationResult);
+	  });
+
+	  var numberOfUsers = 10 + 10 * Math.random();
+	  for (var i3 = 0; i3 < numberOfUsers; i3++) {
+	    var userEmail = "u" + randomString(3 + 5 * Math.random()) + "@gmail.com";
+	    createUser(serverUrl, appAuthKey, userEmail, randomString(1 + 10 * Math.random()), function(userCreationResult) {
+	      console.log("--- # creating user");
+	      console.log(userCreationResult);
+	    });
+	  }
 	});
       }
     });
